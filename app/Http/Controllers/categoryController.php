ea5a9d2f3890ce2ce10d0bd4cad8a0437028bb7f
koushik1808 <koushikmandal189@gmail.com>
2023-10-02 10:07:22 +0530

    update_controller

---
+++ app/Http/Controllers/categoryController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\category;
+use Illuminate\Http\Request;
+
+class categoryController extends Controller
+{
+    //
+    public function view_category(){
+        $category=category::all();
+        $data=compact('category');
+        return  $data;
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\categoryController;
+use App\Http\Controllers\MenuController;
 use App\Http\Controllers\TableController;
 
 /*
@@ -34,6 +36,16 @@ Route::middleware('isLogin')->group(function(){
          Route::get('/Dashbroad','Dashbroad')->name('Admin.Dashbroad');
         //
         Route::get('/Billing/{id}','Billing')->name('Admin.Billing');
+    });
+
+    Route::controller(categoryController::class)->group(function(){
+       //
+       Route::get('/view_category','view_category')->name('Admin.view_category');
+    });
+
+    Route::controller(MenuController::class)->group(function(){
+       //
+       Route::get('/view_menu','view_menu')->name('Admin.view_menu');
     });
 });
 
