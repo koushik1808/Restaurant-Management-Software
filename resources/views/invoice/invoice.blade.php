1119b978cb3a50c1f3433fc544d231356fa4d8f3
Abhijit ghanta <abhijitghanta@hotmail.com>
2023-10-10 14:50:34 +0530

    discount add

--- resources/views/invoice/invoice.blade.php
+++ resources/views/invoice/invoice.blade.php
@@ -20,9 +20,9 @@ Invoice
           <p class="card-title fw-medium text-uppercase" id="in-title">Bihari Bhaiya</p>
           <p class="font-monospace text-end fw-bold">Hotel &amp; Restaurant</p>
           <div class="vstack">
-            <p class="fw-bold">Baluchar (Beside Kalyan Samity)</p>
-            <p class="fw-bold">Satya Choudhury Indoor Stadium Building,</p>
-            <p class="fw-bold">Malda West Bengal</p>
+            <p class="fw-bold m-0">Baluchar (Beside Kalyan Samity)</p>
+            <p class="fw-bold m-0">Satya Choudhury Indoor Stadium Building,</p>
+            <p class="fw-bold m-0">Malda West Bengal</p>
           </div>
           <p class="font-monospace text-start">#Invoice id:- {{$bill}}</p>
           <table class="table">
@@ -56,8 +56,10 @@ Invoice
             </tbody>
           </table>
           <div class="vstack align-items-start">
-            <p>Total Amount:{{$total}}</p>
-            <p>NetPayble:{{$total}}</p>
+            <p class="m-0">Total Amount:{{$total}}</p>
+            <p class="mb-1"> + GST 18 &percnt;</p>
+            <p class="mb-2">Festival Discount 10&percnt;</p>
+            <p class="fw-bold">Net Amount Payable:{{$total}}</p>
             <hr class="w-100 border-2" />
             <p class="fs-2 d-none  w-100 text-center font-monospace " id="message">Thank You Visit Again</p>
           </div>
