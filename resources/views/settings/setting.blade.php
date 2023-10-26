6e0e9f14a904afa8f52ba6ff765c9607114befe1
Abhijit ghanta <abhijitghanta@hotmail.com>
2023-10-26 10:06:33 +0530

    settings modal

--- resources/views/settings/setting.blade.php
+++ resources/views/settings/setting.blade.php
@@ -25,15 +25,15 @@ Settings
 
       <tbody>
         @foreach ($gst as $item)
-            
-       
+
+
         @if ($item->status)
         <tr>
           <td>1</td>
           <td>Gst Invoice</td>
           <td class="fw-medium">Status: <span>ON</span></td>
           <td class="text-center">
-           <a href="{{route('Admin.gstUpdate',['status'=>0])}}"> <button class="btn btn-warning">Off</button></a>
+            <a href="{{route('Admin.gstUpdate',['status'=>0])}}"> <button class="btn btn-warning">Off</button></a>
 
           </td>
         </tr>
@@ -43,42 +43,105 @@ Settings
           <td>Gst Invoice</td>
           <td class="fw-medium">Status: <span>Off</span></td>
           <td class="text-center">
-           <a href="{{route('Admin.gstUpdate',['status'=>1])}}"> <button class="btn btn-primary">On</button></a>
+            <button class="btn btn-primary" data-bs-target="#gst-modal" data-bs-toggle="modal">On</button>
 
           </td>
         </tr>
         @endif
         @endforeach
         @foreach ($discount as $item)
-            
-       
-       @if ($item->status)
-       <tr>
-        <td>2</td>
-        <td>Festival Discount</td>
-        <td class="fw-medium">Status: <span>ON</span></td>
-        <td class="text-center">
-         <a href="{{route('Admin.discountUpdate',['status'=>0])}}"> <button class="btn btn-warning">Off</button></a>
 
-        </td>
-      </tr>
-       @else
-       <tr>
-        <td>2</td>
-        <td>Festival Discount</td>
-        <td class="fw-medium">Status: <span>Off</span></td>
-        <td class="text-center">
-          <a href="{{route('Admin.discountUpdate',['status'=>1])}}"> <button class="btn btn-primary">On</button></a>
 
-        </td>
-      </tr> 
-       @endif
-       @endforeach
-       
-       
+        @if ($item->status)
+        <tr>
+          <td>2</td>
+          <td>Festival Discount</td>
+          <td class="fw-medium">Status: <span>ON</span></td>
+          <td class="text-center">
+            <a href="{{route('Admin.discountUpdate',['status'=>0])}}"> <button class="btn btn-warning">Off</button></a>
+
+          </td>
+        </tr>
+        @else
+        <tr>
+          <td>2</td>
+          <td>Festival Discount</td>
+          <td class="fw-medium">Status: <span>Off</span></td>
+          <td class="text-center">
+            <button class="btn btn-primary" data-bs-target="#discount-modal" data-bs-toggle="modal">On</button>
+
+          </td>
+        </tr>
+        @endif
+        @endforeach
+
+
       </tbody>
 
     </table>
+  </div>
+  {{-- modal for gst --}}
+  <div class="modal fade" id="gst-modal" data-bs-backdrop="static" tabindex="-1" aria-labelledby="exampleModalLabel"
+    aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h1 class="modal-title fs-5" id="exampleModalLabel">Add Gst Informations</h1>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          <form action="">
+            <div class="mb-3">
+              <label for="" class="form-label">
+                Add Gst Number:
+                <input type="text" class="form-control">
+              </label>
+            </div>
+            <div class="mb-3">
+              <label for="" class="form-label">
+                Add Gst Parcent:
+                <input type="text" class="form-control">
+              </label>
+            </div>
+          </form>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-primary">Save changes</button>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  {{-- modal discount --}}
+  <div class="modal fade" id="discount-modal" data-bs-backdrop="static" tabindex="-1"
+    aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h1 class="modal-title fs-5" id="exampleModalLabel">Add Discount Informations</h1>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          <form action="">
+            <div class="mb-3">
+              <label for="" class="form-label">
+                Add Discount Name:
+                <input type="text" class="form-control">
+              </label>
+            </div>
+            <div class="mb-3">
+              <label for="" class="form-label">
+                Add Discount Parcent:
+                <input type="text" class="form-control">
+              </label>
+            </div>
+          </form>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-primary">Save changes</button>
+        </div>
+      </div>
+    </div>
   </div>
 </div>
 @endsection
